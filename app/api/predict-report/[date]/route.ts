3e410cd15805fbe4b5ef586c5b70731c140819d1
Yuan Huang <huangyuan3h@gmail.com>
2024-09-27 17:28:06 +0800

    fix the logic

--- app/api/predict-report/[date]/route.ts
+++ app/api/predict-report/[date]/route.ts
@@ -12,24 +12,33 @@ const fetchStockReportByDate = async (
   date: string
 ): Promise<PredictReportType[]> => {
   try {
-    // Define the filter expression and values
-    const filterExpression = "#fieldName = :fieldValue";
-    const expressionAttributeNames = {
-      "#fieldName": "report_date", // Replace with the actual field name
-    };
-    const expressionAttributeValues = {
-      ":fieldValue": date, // Replace with the value to filter
-    };
+    let hasMoreResults = true;
+    let lastEvaluatedKey = undefined;
+    let allItems: PredictReportType[] = [];
 
-    const command = new ScanCommand({
-      TableName: tableName,
-      FilterExpression: filterExpression,
-      ExpressionAttributeNames: expressionAttributeNames,
-      ExpressionAttributeValues: expressionAttributeValues,
-    });
+    // 循环处理分页
+    while (hasMoreResults) {
+      const command: ScanCommand = new ScanCommand({
+        TableName: tableName,
+        FilterExpression: "#fieldName = :fieldValue",
+        ExpressionAttributeNames: {
+          "#fieldName": "report_date",
+        },
+        ExpressionAttributeValues: {
+          ":fieldValue": date,
+        },
+        ExclusiveStartKey: lastEvaluatedKey, // 设置开始的键值，第一次为空
+      });
 
-    const data = await ddbDocClient.send(command);
-    return data.Items as PredictReportType[];
+      const data = await ddbDocClient.send(command);
+      allItems = allItems.concat(data.Items as PredictReportType[]);
+
+      // 判断是否还有更多数据
+      lastEvaluatedKey = data.LastEvaluatedKey;
+      hasMoreResults = !!lastEvaluatedKey;
+    }
+
+    return allItems;
   } catch (error) {
     console.error("Error fetching filtered data:", error);
     throw error;
